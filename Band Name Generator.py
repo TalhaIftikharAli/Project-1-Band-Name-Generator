aa483aa89f4ad74a8ab6a8184a4edbf6c2c92d89
Talha Iftikar <92679085+TalhaIftikharAli@users.noreply.github.com>
2023-09-17 00:41:46 +0500

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# """
+# Created on Sun Sep 17 00:31:20 2023
+
+# @author: talha.i
+# """
+
+# Band Name Generator i.e. My first project in Python
+
+print('Welcome to the Band Name Generator')
+
+city_name = input("What's the name of the city you grew up in\n")
+
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
